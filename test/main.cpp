b850f92d9800acf04b66ee150bf62e00454902e6
EPTansuo <eptansuo@163.com>
2025-01-12 16:31:31 +0800

    test: add more test

--- test/main.cpp
+++ test/main.cpp
@@ -17,8 +17,13 @@ int main(){
     PString str2("123");
     cout << str2.toInt() << endl;
 
+    PString str_chinese = "你好，世界！";
+    for(size_t i = 0; i < str_chinese.length(); i++){
+        cout << str_chinese.toString() +"[" + std::to_string(i) + "] = " + str_chinese[i] << endl;
+    }
+
     PString str3 = "abc:1222::3333:::";
     PList list = str3.split(":");
-    cout << list << endl;
+    cout << str3.toString() + ".split() = " << list << endl;
     return 0;
 }
