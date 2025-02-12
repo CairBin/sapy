067cdf07b1793e63c61f88158ec3fba525ef924e
EPTansuo <eptansuo@163.com>
2025-02-12 10:42:06 +0800

    Revert "build: prepare for 0.0.2"
    This reverts commit af6b5f4918f543161f1c88afa3905802641c5a3f.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class helloRecipe(ConanFile):
     name = "sapy"
-    version = "0.0.2"
+    version = "0.0.1"
     package_type = "library"
 
     # Optional metadata
